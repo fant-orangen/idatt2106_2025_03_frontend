2df12e2068b67d3913d0e2534aad76c892c3026a
ingrid <ingrimdo@stud.ntnu.no>
2025-04-24 13:53:25 +0200

    SCRUM-340 feat: added a new class for FØR-page, and router path in index.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HeaderNavbar from './components/HeaderNavbar.vue'
+import router from '@/router/index.ts'
+import BeforeCrisis from './components/pages/BeforeCrisis.vue'
 </script>
 
 <template>
   <HeaderNavbar />
+
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
+import BeforeCrisis from '@/components/pages/BeforeCrisis.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: '/før-en-krise', component: BeforeCrisis},
+    
   ],
 })
 
