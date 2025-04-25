7f27c0bfc258e0268a6f6bf53006b2178ba9c8b3
Brahim Helland <brahimh@stud.ntnu.no>
2025-04-25 10:55:00 +0200

    feat: added footer with contact info [SCRUM-357]

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="footer bg-secondary text-secondary-foreground shadow-t-md flex justify-center items-center px-5 py-3 transition-all duration-300 gap-10"
+  >
+    <div>krisefikser.no</div>
+    <div>© IDATT2106 Team 3</div>
+    <div>Contact us at: mageik@stud.ntnu.no</div>
+  </div>
+</template>
