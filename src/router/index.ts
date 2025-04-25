4894dd3f629a95fa316003b94468a71ee5ed381e
Sara <saratag@ntnu.no>
2025-04-25 15:37:02 +0200

    fix: wrong deletion of links and add new link [SCRUM-354]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', component: () => import('@/views/HomeView.vue') },
+  {
+    path: '/info',
+    name: 'Information',
+    component: () => import('@/views/information/EnhancedInformationView.vue')
+  },
   { path: '/login', component: () => import('@/views/LoginView.vue') },
+  { path : '/register', component: () => import('@/views/RegisterView.vue') },
   { path: '/before-crisis', component: () => import('@/views/BeforeCrisisView.vue') },
   { path: '/during-crisis', component: () => import('@/views/DuringCrisisView.vue') },
+  { path: '/household', component: () => import('@/views/HouseholdManager.vue')},
+  { path: '/food-and-drinks', component: () => import('@/views/FoodAndDrinksView.vue')}
 ]
 
 const router = createRouter({
