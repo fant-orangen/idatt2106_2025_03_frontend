5ee8c6d86cd6147e161a114f6e3550db80806b96
Magnus Eik <mageik@stud.ntnu.no>
2025-04-25 11:08:07 +0200

    feat(information):  add routing for information [SCRUM-113]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [{ path: '/', component: () => import('@/views/HomeView.vue') }]
+const routes = [
+  { path: '/', component: () => import('@/views/HomeView.vue') },
+  {
+    path: '/info',
+    name: 'Information',
+    component: () => import('@/views/information/EnhancedInformationView.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
