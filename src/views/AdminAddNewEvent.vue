0e2e4d7cc4e3a3c0f2e008c38f1a35e2b8634444
IMD <144770549+ingriddovre@users.noreply.github.com>
2025-04-26 18:17:43 +0200

    Update AdminAddNewEvent.vue

--- src/views/AdminAddNewEvent.vue
+++ src/views/AdminAddNewEvent.vue
@@ -143,11 +143,7 @@
 			<FormItem>
 				<FormLabel>{{$t('add-event-info.titles.description')}}:</FormLabel>
 				<FormControl>
-					<textarea class="
-				'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-				'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-				'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-				" style="min-height:100px;" placeholder="Description" v-bind="field"></textarea>
+					<Textarea placeholder="Description" v-bind="field"></Textarea>
 				</FormControl>
 				<FormDescription>{{ $t('add-event-info.description') }}</FormDescription>
 				<FormMessage v-if="meta.touched  || meta.submitFailed">{{ errorMessage }}</FormMessage>
@@ -180,6 +176,7 @@
 	import * as z from 'zod'
 	import { useI18n } from 'vue-i18n';
 	import { Input } from '@/components/ui/input'
+  import { Textarea } from '@/components/ui/textarea'
 	import {
 		FormControl,
 		FormDescription,
