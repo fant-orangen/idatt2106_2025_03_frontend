5b3655d662e8341bdcfcf6cbb8254d2bd18779be
IMD <144770549+ingriddovre@users.noreply.github.com>
2025-04-26 18:19:08 +0200

    Update AdminAddNewPOI.vue

--- src/views/AdminAddNewPOI.vue
+++ src/views/AdminAddNewPOI.vue
@@ -138,11 +138,7 @@
 			<FormItem>
 				<FormLabel>{{$t('add-event-info.titles.description')}}:</FormLabel>
 				<FormControl>
-					<textarea class="
-				'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-				'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-				'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-				" style="min-height:100px;" placeholder="Description" v-bind="field"></textarea>
+					<Textarea placeholder="Description" v-bind="field"></Textarea>
 				</FormControl>
 				<FormDescription>{{ $t('add-POI-info.info.description') }}</FormDescription>
 				<FormMessage v-if="meta.touched  || meta.submitFailed">{{ errorMessage }}</FormMessage>
@@ -169,6 +165,7 @@ import { useI18n } from 'vue-i18n';
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { toTypedSchema } from '@vee-validate/zod'
+import { Textarea } from '@/components/ui/textarea'
 import * as z from 'zod'
 import {
 	Breadcrumb,
