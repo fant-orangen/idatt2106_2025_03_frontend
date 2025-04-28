b7ff1206c0f6559b1feef7854c3d15595f2fa984
Brahim Helland <brahimh@stud.ntnu.no>
2025-04-28 15:23:24 +0200

    feat: fixed added forgot password and show/hide password functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HeaderNavbar from './components/HeaderNavbar.vue'
 import Footer from './components/Footer.vue'
+import { Toaster } from './components/ui/sonner'
 import InvitationPopup from './components/invitation/InvitationPopup.vue'
 import { RouterView } from 'vue-router'
 </script>
@@ -8,6 +9,7 @@ import { RouterView } from 'vue-router'
 <template>
   <div class="min-h-screen flex flex-col">
     <HeaderNavbar />
+    <Toaster class="" />
     <div class="flex-grow">
       <RouterView />
     </div>
