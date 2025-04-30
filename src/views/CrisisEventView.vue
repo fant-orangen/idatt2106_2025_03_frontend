e5d06043efb249cbd42ae1b0a62ed9e23353fd60
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2025-04-30 11:21:24 +0200

    feat: Add view for crisis event.

---
+++ src/views/CrisisEventView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <CrisisEventOverview />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CrisisEventOverview from '@/components/crisis/CrisisEventOverview.vue';
+</script>
