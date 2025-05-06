e13070d5775898c45e223aa3abb7a53940ab1210
Brahim Helland <brahimh@stud.ntnu.no>
2025-05-06 17:12:27 +0200

    feat: added empty add new quiz button [SCRUM-201]

--- src/components/gamification/quiz/QuizOverview.vue
+++ src/components/gamification/quiz/QuizOverview.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
-import { quizService } from '@/services/QuizService.ts'
-import { QuizPreview } from '@/models/Quiz'
+import { ref, onMounted } from 'vue'
+import { Plus } from 'lucide-vue-next'
+import type { QuizPreview } from '@/models/Quiz'
 import InfiniteScroll from '@/components/ui/InfiniteScroll.vue'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-vue-next'
 import { useQuizStore } from '@/stores/QuizStore.ts'
+import { useUserStore } from '@/stores/UserStore.ts'
 import {
   Card,
   CardHeader,
@@ -14,10 +15,12 @@ import {
   CardContent,
   CardFooter,
 } from '@/components/ui/card'
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
 import Button from '@/components/ui/button/Button.vue'
 
 const quizzes = ref<QuizPreview[]>([]) // Explicitly define the type
 const quizStore = useQuizStore() // Using the QuizStore
+const userStore = useUserStore() // Using the UserStore
 
 onMounted(() => {
   quizStore.fetchAllActiveQuizzes()
@@ -37,7 +40,6 @@ const scrollToTop = () => {
 <template>
   <div class="flex flex-col items-center">
     <h1 class="text-2xl font-bold m-10">Quizzes</h1>
-
     <!-- Search Input -->
     <div class="relative w-full max-w-2xl items-center">
       <Input
@@ -57,7 +59,7 @@ const scrollToTop = () => {
       loadingText="Loading more quizzes..."
       endMessage="No more quizzes to load"
       @load-more="quizStore.fetchAllActiveQuizzes(quizStore.currentPage + 1)"
-      class="w-full max-w-2xl mt-10"
+      class="w-full max-w-2xl mb-10 mt-10"
     >
       <ul class="space-y-4">
         <li v-for="quiz in quizStore.filteredQuizzes" :key="quiz.id">
@@ -86,4 +88,17 @@ const scrollToTop = () => {
       Back to top
     </Button>
   </div>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <Button
+          v-if="userStore.isAdminUser"
+          class="fixed h-15 w-15 bottom-15 left-15 z-100 rounded-full flex items-center justify-center"
+        >
+          <Plus :size="40" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent> CREATE A NEW QUIZ </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
 </template>
