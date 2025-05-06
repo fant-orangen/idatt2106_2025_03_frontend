73c777ad19a4ccec88a6afe4401b00f8ffefa3ad
ingrid <ingrimdo@stud.ntnu.no>
2025-05-06 17:39:14 +0200

    feat: new file for creating news articles [SCRUM-495]

--- src/views/AdminAddNews.vue
+++ src/views/AdminAddNews.vue
@@ -2,6 +2,32 @@
 <div>
   <h1>Title</h1>
 
+    <Card>
+      <CardHeader>
+        <CardTitle>New news article</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        Card Content
+      </CardContent>
+      <CardFooter>
+        <Button >Submit</Button>
+      </CardFooter>
+    </Card>
+
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        Card Content
+      </CardContent>
+      <CardFooter>
+        Card Footer
+      </CardFooter>
+    </Card>
+
 
 </div>
 
@@ -11,6 +37,14 @@
 </template>
 
 <script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 
 </script>
