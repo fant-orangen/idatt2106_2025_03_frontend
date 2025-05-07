cba07bbf733132d21c00abc1d7cc992e1c2ddd28
Brahim Helland <brahimh@stud.ntnu.no>
2025-05-07 11:49:38 +0200

    feat(quiz-admin): added admin buttons for editing and deleting quiz [SCRUM-201]

--- src/components/gamification/quiz/QuizOverview.vue
+++ src/components/gamification/quiz/QuizOverview.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { Plus } from 'lucide-vue-next'
+import { Plus, Trash2, Pencil } from 'lucide-vue-next'
 import type { QuizPreview } from '@/models/Quiz'
 import InfiniteScroll from '@/components/ui/InfiniteScroll.vue'
 import { Input } from '@/components/ui/input'
@@ -16,6 +16,17 @@ import {
   CardFooter,
 } from '@/components/ui/card'
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 import Button from '@/components/ui/button/Button.vue'
 
 const quizzes = ref<QuizPreview[]>([]) // Explicitly define the type
@@ -35,11 +46,28 @@ const scrollToTop = () => {
     behavior: 'smooth', // Smooth scrolling
   })
 }
+
+const handleEdit = (quizId: number) => {
+  console.log(`Edit quiz with ID: ${quizId}`)
+}
+
+const deleteQuiz = (quizId: number) => {
+  if (!userStore.isAdminUser) {
+    console.error('User is not authorized to delete quizzes')
+    return
+  }
+  console.log(`Delete quiz with ID: ${quizId}`)
+  quizStore.deleteQuiz(quizId)
+  quizStore.fetchAllActiveQuizzes()
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center">
     <h1 class="text-2xl font-bold m-10">Quizzes</h1>
+    <Button v-if="userStore.isAdminUser" class="mb-10">
+      <Plus :size="40" /> Create new quiz
+    </Button>
     <!-- Search Input -->
     <div class="relative w-full max-w-2xl items-center">
       <Input
@@ -63,7 +91,9 @@ const scrollToTop = () => {
     >
       <ul class="space-y-4">
         <li v-for="quiz in quizStore.filteredQuizzes" :key="quiz.id">
-          <Card>
+          <Card
+            class="hover:shadow-lg hover:shadow-gray-300 dark:hover:shadow-gray-800 transition-shadow duration-300"
+          >
             <CardHeader>
               <CardTitle>
                 {{ quiz.name }}
@@ -72,9 +102,56 @@ const scrollToTop = () => {
             </CardHeader>
             <CardContent> Last attempt: </CardContent>
             <CardFooter>
-              <Button>
-                {{ $t('gamification.quiz.takeQuiz') }}
-              </Button>
+              <div class="flex w-full justify-between items-center">
+                <Button>
+                  {{ $t('gamification.quiz.takeQuiz') }}
+                </Button>
+                <div>
+                  <TooltipProvider :delayDuration="350">
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Button
+                          v-if="userStore.isAdminUser"
+                          size="icon"
+                          variant="ghost"
+                          @click="handleEdit(quiz.id)"
+                        >
+                          <Pencil :size="20" />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent> Edit Quiz </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <TooltipProvider :delayDuration="350">
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <AlertDialog>
+                          <AlertDialogTrigger as-child>
+                            <Button v-if="userStore.isAdminUser" size="icon" variant="ghost">
+                              <Trash2 class="h-5 w-5" />
+                            </Button>
+                          </AlertDialogTrigger>
+                          <AlertDialogContent>
+                            <AlertDialogHeader>
+                              <AlertDialogTitle>{{ $t('alert.areYouSure') }}</AlertDialogTitle>
+                              <AlertDialogDescription>
+                                {{ $t('alert.deleteQuiz') }}
+                              </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                              <AlertDialogCancel variant="destructive">Cancel</AlertDialogCancel>
+                              <AlertDialogAction @click="deleteQuiz(quiz.id)"
+                                >Continue</AlertDialogAction
+                              >
+                            </AlertDialogFooter>
+                          </AlertDialogContent>
+                        </AlertDialog>
+                      </TooltipTrigger>
+                      <TooltipContent> Delete Quiz </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </div>
+              </div>
             </CardFooter>
           </Card>
         </li>
@@ -88,17 +165,4 @@ const scrollToTop = () => {
       Back to top
     </Button>
   </div>
-  <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger>
-        <Button
-          v-if="userStore.isAdminUser"
-          class="fixed h-15 w-15 bottom-15 left-15 z-100 rounded-full flex items-center justify-center"
-        >
-          <Plus :size="40" />
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent> CREATE A NEW QUIZ </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
 </template>
