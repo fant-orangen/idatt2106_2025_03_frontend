5f3f53009b81209b33a18cd7fd6975f024f2d076
Brahim Helland <brahimh@stud.ntnu.no>
2025-05-07 17:32:30 +0200

    feat: added missing views [SCRUM-201]

--- src/views/gamification/admin/EditQuizView.vue
+++ src/views/gamification/admin/EditQuizView.vue
@@ -1,1 +1,36 @@
-w
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import AdminEditQuiz from '@/components/gamification/quiz/AdminEditQuiz.vue'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps<{ quizId: number }>()
+console.log('Quiz ID:', props.quizId)
+</script>
+
+<template>
+  <Breadcrumb class="m-5">
+    <BreadcrumbList>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/"> Home </BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/quizzes"> Quiz Overview </BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage> Quiz Creator </BreadcrumbPage>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+
+  <AdminEditQuiz :quizId="props.quizId" />
+</template>
